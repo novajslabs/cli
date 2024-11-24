b841292efc8de3810a0acc225405909e57744969
Daniel Castillo <dlcastillo3015@gmail.com>
2024-11-24 07:37:38 -0500

    feat: show spinner when adding hooks #23

--- src/commands/vite.ts
+++ src/commands/vite.ts
@@ -3,17 +3,15 @@ import {downloadHook, getAllHooksName} from "@/src/utils/hook-actions";
 import {getProjectLanguageExtension} from "@/src/utils/get-project-info";
 import {mkdirp} from "fs-extra";
 import {logger} from "@/src/utils/logger";
+import ora from "ora"
 
 export const vite = new Command()
     .name("vite")
     .description("add hooks to your Vite + React project")
-    .action(async (hooks, opts) => {
+    .action(async () => {
         const PATH_FOR_HOOKS = "src/hooks";
 
-        // Handle TypeScript or JavaScript
         const extension = getProjectLanguageExtension();
-
-        // Handle hooks selection
         const allHooks = await getAllHooksName();
 
         try {
@@ -23,9 +21,11 @@ export const vite = new Command()
             process.exit(1);
         }
 
-        for (let i = 0; i < allHooks.length; i++) {
-            const hook = allHooks[i];
+        const spinner = ora(`Adding hooks...`).start()
 
+        for (const hook of allHooks) {
             await downloadHook(hook, PATH_FOR_HOOKS, extension);
         }
+
+        spinner.succeed("Hooks added successfully.");
     });
--- src/utils/hook-actions.ts
+++ src/utils/hook-actions.ts
@@ -1,68 +1,67 @@
 import fs from "fs/promises";
 import fetch from "node-fetch";
-import { logger } from "@/src/utils/logger";
+import {logger} from "@/src/utils/logger";
 
 interface HookList {
-  name: string;
-  path: string;
-  sha: string;
-  size: number;
-  url: string;
-  html_url: string;
-  git_url: string;
-  download_url: string;
-  type: string;
-  _links: {
-    self: string;
-    git: string;
-    html: string;
-  };
+    name: string;
+    path: string;
+    sha: string;
+    size: number;
+    url: string;
+    html_url: string;
+    git_url: string;
+    download_url: string;
+    type: string;
+    _links: {
+        self: string;
+        git: string;
+        html: string;
+    };
 }
 
 export const getAllHooksName = async () => {
-  const allHooksUrl =
-    "https://api.github.com/repos/novajslabs/nova.js/contents/src/hooks/ts";
+    const allHooksUrl =
+        "https://api.github.com/repos/novajslabs/nova.js/contents/src/hooks/ts";
 
-  try {
-    const response = await fetch(allHooksUrl);
+    try {
+        const response = await fetch(allHooksUrl);
 
-    if (!response.ok) {
-      throw Error;
+        if (!response.ok) {
+            throw Error;
+        }
+
+        const hooksData = (await response.json()) as HookList[];
+
+        return hooksData.map((hook) => hook.name.replace(".ts", ""));
+    } catch (e) {
+        logger.error("Error obtaining the list of hooks. Try again.");
+        process.exit(1);
     }
-
-    const hooksData = (await response.json()) as HookList[];
-
-    return hooksData.map((hook) => hook.name.replace(".ts", ""));
-  } catch (e) {
-    logger.error("Error obtaining the list of hooks. Try again.");
-    process.exit(1);
-  }
 };
 
 export const downloadHook = async (
-  hookName: string,
-  path: string,
-  type: "ts" | "js"
+    hookName: string,
+    path: string,
+    type: "ts" | "js"
 ) => {
-  const hookUrl = `https://raw.githubusercontent.com/novajslabs/nova.js/main/src/hooks/${type}/${hookName}.${type}`;
+    const hookUrl = `https://raw.githubusercontent.com/novajslabs/nova.js/main/src/hooks/${type}/${hookName}.${type}`;
 
-  try {
-    const response = await fetch(hookUrl);
+    try {
+        const response = await fetch(hookUrl);
 
-    if (!response.ok) {
-      if (response.status === 404) {
-        throw new Error(`The ${hookName} hook does not exist.`);
-      } else {
-        throw new Error(`An error occurred adding ${hookName}. Try again.`);
-      }
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error(`The ${hookName} hook does not exist.`);
+            } else {
+                throw new Error(`An error occurred adding ${hookName}. Try again.`);
+            }
+        }
+
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        await fs.writeFile(`${path}/${hookName}.${type}`, buffer);
+    } catch (e) {
+        logger.error(e);
     }
-
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-
-    await fs.writeFile(`${path}/${hookName}.${type}`, buffer);
-    logger.success(`Added ${hookName}`);
-  } catch (e) {
-    logger.error(e);
-  }
 };
