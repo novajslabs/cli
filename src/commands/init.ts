9b63a672c74c5a331cb58221c992eb754b3446cb
Daniel Castillo <dlcastillo3015@gmail.com>
2024-11-24 12:56:56 -0500

    feat: add overwrite option #23

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -10,15 +10,28 @@ import {
 import {mkdirp} from "fs-extra";
 import {logger} from "@/src/utils/logger";
 import ora from "ora"
+import {z} from "zod";
+import {existsSync} from "fs";
+import chalk from "chalk";
+import prompts from "prompts";
 
 const DEFAULT_PATH = "src/hooks";
 const APP_WITHOUT_SRC_PATH = "app/hooks";
 const PAGES_WITHOUT_SRC_PATH = "pages/hooks";
 
+const addOptionsSchema = z.object({
+    overwrite: z.boolean().optional(),
+});
+
 export const init = new Command()
     .name("init")
     .description("add React hooks to your Vite, Next.js or Astro project")
-    .action(async () => {
+    .option("-o, --overwrite", "overwrite existing files")
+    .action(async (opts) => {
+        const options = addOptionsSchema.parse({
+            ...opts,
+        });
+
         const projectTechStack = getProjectTechStack();
         if (!projectTechStack) {
             logger.error("Project tech stack not detected. Exiting.");
@@ -53,9 +66,33 @@ export const init = new Command()
             process.exit(1);
         }
 
-        const spinner = ora(`Adding hooks...`).start();
+        const spinner = ora("Adding hooks...").start();
 
         for (const hook of allHooks) {
+            // Handle overwrite
+            if (
+                options.overwrite === undefined &&
+                existsSync(`${path}/${hook}.${extension}`)
+            ) {
+                spinner.stop();
+                const {overwrite} = await prompts({
+                    type: "confirm",
+                    name: "overwrite",
+                    message: `Hook ${hook} already exists. Would you like to overwrite?`,
+                    initial: false,
+                });
+
+                if (!overwrite) {
+                    logger.info(
+                        `Skipped ${hook}. To overwrite, run with the ${chalk.green(
+                            "--overwrite"
+                        )} flag.`
+                    );
+                    continue;
+                }
+            }
+
+            spinner.start("Adding hooks...");
             await downloadHook(hook, path, extension);
         }
 
